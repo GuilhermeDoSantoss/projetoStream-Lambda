001bc1ce18920b39071859f4f6916d19dd102a93
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-02-23 15:14:20 -0300

    projeto criado

---
+++ src/main/java/com/santos/stream_lambda/StreamLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.santos.stream_lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamLambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santos/stream_lambda/StreamLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santos.stream_lambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
